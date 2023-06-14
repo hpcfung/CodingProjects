bc999d3052605fd25b16fc968ddb296d99be1b2f
hpcfung <91354621+hpcfung@users.noreply.github.com>
2023-06-14 00:43:46 -0400

    Update energy_plot.py

--- 2023/USRA/qmc_script/processing/energy_plot.py
+++ 2023/USRA/qmc_script/processing/energy_plot.py
@@ -5,44 +5,48 @@ import matplotlib.pyplot as plt
 def read_arr(file_path,batch_idx):
     file_path = file_path + '/batch_'+str(batch_idx).zfill(num_digits)+'_raw_observables.csv'
     raw_arr = np.genfromtxt(file_path, delimiter=',')
-    arr = np.delete(raw_arr, 0, axis=0)  # remove first row batch,n_sso,n_ops; nan nan nan
+    arr = np.delete(raw_arr, 0, axis=0)  # remove first row = batch,n_sso,n_ops; nan nan nan
     print(f"arr shape ={arr.shape}")
     return arr
 
-def get_energy_shift():
-    file_path = 'seed=1234/energy_shift.csv'
+def get_energy_shift(file_path):
+    file_path = file_path + '/energy_shift.csv'
     raw_arr = np.genfromtxt(file_path, delimiter=',')
     return raw_arr.item()
 
 def get_beta_list():
     for (dirpath, dirnames, filenames) in os.walk(file_root):
-        beta_list = dirnames
+        beta_list = dirnames # the first tuple contains all immediate directories within directory
         break
     return [w[5:] for w in beta_list]
 
 def get_energy(j):
+    """
+    Returns the energy of the QMC run at the jth beta in beta_list
+    Formula: energy = -mean(n_ops) / beta + energy_shift
+    """
     file_path = file_root + '/beta=' + beta_list[j] + '/seed=' + seed
     print(file_path)
     for k in range(1,batch+1): # batch 0 is equilibration
         if k == 1:
             tmp_arr = read_arr(file_path,k)
         else:
-            tmp_arr = np.concatenate((tmp_arr, read_arr(k)), axis=0)
+            tmp_arr = np.concatenate((tmp_arr, read_arr(file_path,k)), axis=0) # merge arrays from different batches
 
     print(tmp_arr)
     print(tmp_arr.shape)
 
-    n_ops = tmp_arr[:,2]
+    n_ops = tmp_arr[:,2] # cols: batch,n_sso,n_ops
 
-    energy_shift = get_energy_shift()
+    energy_shift = get_energy_shift(file_path)
     print(f"energy shift = {energy_shift}")
 
-    energy = -np.mean(n_ops) / beta + energy_shift
-    print(energy)
+    energy = -np.mean(n_ops) / beta_num_list[j] + energy_shift
+    print(f"energy = {energy}")
+    return energy
 
 if __name__ == "__main__":
     batch = 10
-    beta = 0.5
     file_root = 'delta=-0.36'
     seed = '1234'
 
@@ -58,7 +62,13 @@ if __name__ == "__main__":
     energy_list = []
     for j in range(len(beta_list)):
         energy_list.append(get_energy(j))
+    print(energy_list)
 
-    # plt.plot([2,1,3],[4,1,9]) # (1,1), (2,4), (3,9)
-    # plt.show()
+    results = []
+    for i in range(len(beta_list)):
+        results.append((beta_num_list[i],energy_list[i]))
+    print(f"(beta,energy) = {results}")
+
+    plt.plot(beta_num_list,energy_list)
+    plt.show()
 
