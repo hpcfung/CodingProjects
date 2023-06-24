5ea9e868cfc45b7ce91bf1b0b81fc35dda57733a
hpcfung <91354621+hpcfung@users.noreply.github.com>
2023-06-24 06:48:27 -0400

    Update checking.py

--- 2023/USRA/qmc_script/checking.py
+++ 2023/USRA/qmc_script/checking.py
@@ -1,4 +1,4 @@
-import sys
+import sys, re
 
 def check_file(file_name):
     file1 = open(file_name, 'r')
@@ -8,6 +8,8 @@ def check_file(file_name):
         # print(repr(line)) # \n is printed literally
         if line.startswith("Starting task"):
             print(line[9:-1]) # \n is not printed (counted as 1 char)
+            task_num = int(re.search(r"[a-zA-Z\s]*([0-9]*)\n", line).group(1))
+            job_list[task_num] = True # this job is present
 
         for error_word in error_keywords_list:
             if error_word.casefold() in line.casefold():
@@ -38,15 +40,24 @@ if __name__ == "__main__":
     Error examples:
     slurmstepd: error: *** JOB 6800453 ON gra812 CANCELLED AT 2023-05-26T02:17:00 DUE TO TIME LIMIT ***
     slurmstepd: error: Detected 1 oom-kill event(s) in StepId=6800197.batch. Some of your processes may have been killed by the cgroup out-of-memory handler.
+    
+    Protocol: For each L,
     """
 
     job_id = 7228219
     job_num = 270
-    job_name = "L20_prod_test-"
+    job_name = "out/7228219/L11_prod_test-" # "L20_prod_test-"
 
     error_keywords_list = ["slurmstepd", "error", "CANCEL", "LIMIT", "kill"]
 
+    job_list = [False] * job_num
     for k in range(job_id,job_id+job_num):
         check_file(file_name=job_name+str(k)+".out")
         # sys.exit() # first .out only
+    
+    if all(job_list):
+        print(f"All {job_num} tasks are present.")
+    else:
+        missing_jobs = [ID for ID in range(job_num) if job_list[x] == False]
+        print(f"Missing tasks = {missing_jobs}")
     print("All programs are complete. No error keywords found.")
