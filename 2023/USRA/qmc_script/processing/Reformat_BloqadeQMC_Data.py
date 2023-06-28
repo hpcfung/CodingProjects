3ddbf6334e45fed43088eb58e40024d02b586a0c
hpcfung <91354621+hpcfung@users.noreply.github.com>
2023-06-28 19:44:47 -0400

    Update Reformat_BloqadeQMC_Data.py

--- 2023/USRA/qmc_script/processing/Reformat_BloqadeQMC_Data.py
+++ 2023/USRA/qmc_script/processing/Reformat_BloqadeQMC_Data.py
@@ -2,20 +2,11 @@
 
 import os
 import sys
-
 import copy
-
 import re
 
 import itertools as itr
-
 import numpy as np
-
-# import matplotlib
-# from matplotlib import colors, cm, patches
-# import matplotlib.pyplot as plt
-# from mpl_toolkits.axes_grid1 import make_axes_locatable
-
 import networkx as nx
 
 import torch
@@ -42,210 +33,131 @@ from rydberggpt.data.utils_graph import (
 import pandas as pd
 import json
 
-#######################################################################################
-
-# Package parameters
-
-# matplotlib.rcParams["figure.figsize"] = (12, 8)
-# matplotlib.rcParams["font.size"] = 30
-# matplotlib.rcParams["text.usetex"] = True
-# matplotlib.rcParams["mathtext.fontset"] = "stix"
-# matplotlib.rcParams["font.family"] = "STIXGeneral"
-
 
 ########################################################################################
-
+def process_qmc_batch(batch_dir):
+    """
+    Reads batch 1 to 10 in batch_dir
+    Returns concatenated numpy array
+    Does processing: decorrelation, snake path
+    """
+    measurements_list = []
+    for i in range(1,max_batch+1): # batch 1 to max_batch
+        sample_path = os.path.join(batch_dir,"batch_"+str(i).zfill(batch_digits)+"_samples.csv")
+        batch_measurements = np.genfromtxt(sample_path, delimiter=",", dtype=str)
+        batch_measurements = np.array([i.split(" ") for i in batch_measurements]).astype(
+            float
+        )
+        # decorrelate
+        batch_measurements = batch_measurements[::decorrelation_rate,:]
+        measurements_list.append(batch_measurements)
+    measurements = np.concatenate(measurements_list, axis=0)    
+    print(measurements)
+    print(measurements.shape)
+    sys.exit()
 
 def process_qmc_data(qmc_path, save_path):
     """
-    Expects eg batch_02_samples.csv
-    csv file may be in nested directories
+
     """
 
     # data_path = qmc_path # os.path.join(qmc_path, "data")
-    data_path = "/SCRATCHTAINER/qmc_data/batch_01_samples.csv"
     # data_path = "$HOME/scratch/qmc_data/batch_01_samples.csv"
     # data_path = "/home/hpcfung/scratch/qmc_data/batch_01_samples.csv"
-    # L=5, delta=-0.36, beta=0.5
-    # normally data_path used to find sample_path
 
-    dfs = []
-    filenames = []
-    # TMP BEGINS
-    sample_path = data_path # "/home/hpcfung/scratch/qmc_data/batch_01_samples.csv" # os.path.join(directory, file)
+    for I, rb_str in enumerate(rb_dict):
+        for J, delta_str in enumerate(delta_dict):
+            for K, beta_str in enumerate(beta_dict):
+                batch_dir = os.path.join(qmc_path,"Rb="+rb_str,"omega="+omega_str,
+                               "delta="+delta_str,"beta="+beta_str,
+                               "seed="+seed_str)
+                process_qmc_batch(batch_dir=batch_dir)
+                sys.exit()
+    sys.exit()
 
-    filenames.append(sample_path)
+    for directory, subdirlist, filelist in os.walk(data_path):
+        for file in filelist:
+            if file.endswith("samples.csv"):
 
-    # m = re.findall(r"(\w+)=([+-]?[0-9]*[.]?[0-9]*)", sample_path)
+                sample_path = os.path.join(directory, file)
 
-    L = 5 # int(m[0][1])
-    delta = -0.364386792453 # float(m[1][1])
-    Rb = 1.05
-    beta = 64.0
 
-    print(
-        "{:<80}".format(
-            "\rCurrently processing L={}, delta={} data".format(L, delta)
-        ),
-        end="\n",
-    )
-    sys.stdout.flush()
+                m = re.findall(r"(\w+)=([+-]?[0-9]*[.]?[0-9]*)", sample_path)
 
-    ########################################################################################
+                L = int(m[0][1])
+                delta = float(m[1][1])
 
-    measurements = np.genfromtxt(sample_path, delimiter=",", dtype=str)
-    measurements = np.array([i.split(" ") for i in measurements]).astype(
-        float
-    )
+                print(
+                    "{:<80}".format(
+                        "\rCurrently processing L={}, delta={} data".format(L, delta)
+                    ),
+                    end="\n",
+                )
+                sys.stdout.flush()
 
-    df = pd.DataFrame(
-        {"measurement": list(measurements)},
-        index=range(len(measurements)),
-    )
+                ########################################################################################
 
-    dfs.append(df)
+                measurements = np.genfromtxt(sample_path, delimiter=",", dtype=str)
+                measurements = np.array([i.split(" ") for i in measurements]).astype(
+                    float
+                )
 
-    ########################################################################################
+                df = pd.DataFrame(
+                    {"measurement": list(measurements)},
+                    index=range(len(measurements)),
+                )
 
-    graph_config = GridGraph(
-        num_atoms=L**2,
-        graph_name="grid_graph",
-        Rb=Rb,
-        delta=delta,
-        omega=1.0,
-        beta=beta,
-        n_rows=L,
-        n_cols=L,
-    )
-    graph = get_graph(graph_config)
-    graph_dict = graph_to_dict(graph)
 
-    ########################################################################################
+                ########################################################################################
 
-    config_dict = dict(
-        omega=graph_config.omega,
-        lx=graph_config.n_cols,
-        ly=graph_config.n_rows,
-        beta=graph_config.beta,
-        num_atoms=graph_config.num_atoms,
-        delta=graph_config.delta,
-        Rb=graph_config.Rb,
-    )
+                graph_config = GridGraph(
+                    num_atoms=L,
+                    graph_name="grid_graph",
+                    Rb=1.2,
+                    delta=delta,
+                    omega=1.0,
+                    beta=1.0,
+                    n_rows=1,
+                    n_cols=L,
+                )
+                graph = get_graph(graph_config)
+                graph_dict = graph_to_dict(graph)
 
-    ########################################################################################
+                ########################################################################################
 
-    if save_path is not None:
+                config_dict = dict(
+                    omega=graph_config.omega,
+                    lx=graph_config.n_cols,
+                    ly=graph_config.n_rows,
+                    beta=graph_config.beta,
+                    num_atoms=graph_config.num_atoms,
+                    delta=graph_config.delta,
+                    Rb=graph_config.Rb,
+                )
 
-        if not os.path.isdir(save_path):
-            os.makedirs(save_path)
+                ########################################################################################
 
-        folder_path = os.path.join(
-            save_path, "QMC_L={}_delta={}".format(L, delta)
-        )
-        os.mkdir(folder_path)
+                if save_path is not None:
 
-        df.to_hdf(
-            os.path.join(folder_path, "dataset.h5"), key="data", mode="w"
-        )
-        save_graph_to_json(
-            graph_dict, os.path.join(folder_path, "graph.json")
-        )
+                    if not os.path.isdir(save_path):
+                        os.makedirs(save_path)
 
-        with open(os.path.join(folder_path, "config.json"), "w") as f:
-            json.dump(config_dict, f)
+                    folder_path = os.path.join(
+                        save_path, "EssiQQurke_L={}_delta={}".format(L, delta)
+                    )
+                    os.mkdir(folder_path)
 
-    # TMP ENDS
+                    df.to_hdf(
+                        os.path.join(folder_path, "dataset.h5"), key="data", mode="w"
+                    )
+                    save_graph_to_json(
+                        graph_dict, os.path.join(folder_path, "graph.json")
+                    )
 
-    # for I, rb in enumerate(rb_dict):
-    #     for J, delta in enumerate(delta_dict):
-    #         for K, beta in enumerate(beta_dict):
-                
+                    with open(os.path.join(folder_path, "config.json"), "w") as f:
+                        json.dump(config_dict, f)
 
-    # for directory, subdirlist, filelist in os.walk(data_path):
-    #     for file in filelist:
-    #         if file.endswith("samples.csv"):
-
-    #             sample_path = os.path.join(directory, file)
-
-    #             filenames.append(sample_path)
-
-    #             m = re.findall(r"(\w+)=([+-]?[0-9]*[.]?[0-9]*)", sample_path)
-
-    #             L = int(m[0][1])
-    #             delta = float(m[1][1])
-
-    #             print(
-    #                 "{:<80}".format(
-    #                     "\rCurrently processing L={}, delta={} data".format(L, delta)
-    #                 ),
-    #                 end="\n",
-    #             )
-    #             sys.stdout.flush()
-
-    #             ########################################################################################
-
-    #             measurements = np.genfromtxt(sample_path, delimiter=",", dtype=str)
-    #             measurements = np.array([i.split(" ") for i in measurements]).astype(
-    #                 float
-    #             )
-
-    #             df = pd.DataFrame(
-    #                 {"measurement": list(measurements)},
-    #                 index=range(len(measurements)),
-    #             )
-
-    #             dfs.append(df)
-
-    #             ########################################################################################
-
-    #             graph_config = GridGraph(
-    #                 num_atoms=L,
-    #                 graph_name="grid_graph",
-    #                 Rb=1.2,
-    #                 delta=delta,
-    #                 omega=1.0,
-    #                 beta=1.0,
-    #                 n_rows=1,
-    #                 n_cols=L,
-    #             )
-    #             graph = get_graph(graph_config)
-    #             graph_dict = graph_to_dict(graph)
-
-    #             ########################################################################################
-
-    #             config_dict = dict(
-    #                 omega=graph_config.omega,
-    #                 lx=graph_config.n_cols,
-    #                 ly=graph_config.n_rows,
-    #                 beta=graph_config.beta,
-    #                 num_atoms=graph_config.num_atoms,
-    #                 delta=graph_config.delta,
-    #                 Rb=graph_config.Rb,
-    #             )
-
-    #             ########################################################################################
-
-    #             if save_path is not None:
-
-    #                 if not os.path.isdir(save_path):
-    #                     os.makedirs(save_path)
-
-    #                 folder_path = os.path.join(
-    #                     save_path, "EssiQQurke_L={}_delta={}".format(L, delta)
-    #                 )
-    #                 os.mkdir(folder_path)
-
-    #                 df.to_hdf(
-    #                     os.path.join(folder_path, "dataset.h5"), key="data", mode="w"
-    #                 )
-    #                 save_graph_to_json(
-    #                     graph_dict, os.path.join(folder_path, "graph.json")
-    #                 )
-
-    #                 with open(os.path.join(folder_path, "config.json"), "w") as f:
-    #                     json.dump(config_dict, f)
-
-    return dfs, filenames
+    return
 
 
 ########################################################################################
@@ -256,7 +168,7 @@ save_path = os.path.abspath("/SCRATCHTAINER/qmc_data/tmp_data")
 # "/home/hpcfung/scratch/qmc_data/tmp_data"
 # os.path.abspath("/home/hpcfung/scratch/qmc_data/data")
 
-qmc_path = "/home/hpcfung/scratch/qmc_data/L=5"
+qmc_path = "/SCRATCHTAINER/qmc_data/L=5"
 
 rb_dict = {"1.05": 1.05, "1.15": 1.15, "1.30": 1.30}
 delta_dict = {"-0.36": -0.364386792453,
@@ -279,64 +191,14 @@ beta_dict = {"0.5": 0.5,
              "48.0": 48.0,
              "64.0": 64.0}
 
-########################################################################################
+omega_str = "1.00"
+seed_str = "1234"
 
-dfs, f = process_qmc_data(qmc_path, save_path=save_path)
-sys.exit()
+max_batch = 10
+batch_digits = len(str(max_batch))
 
-# below: probably observables plots from QMC data
-########################################################################################
-
-i = 6
-print("Using dataset: {}".format(f[i]))
-
-x = dfs[i].measurement
-x = np.array(list(x)).astype(float)
-
-plt.imshow(x[:10])
+decorrelation_rate = 10
 
 ########################################################################################
 
-
-L = 8
-delta = []
-x = []
-for i, s in enumerate(f):
-    m = re.findall(r"(\w+)=([+-]?[0-9]*[.]?[0-9]*)", s)
-
-    if int(m[0][1]) != 8:
-        continue
-    else:
-        delta.append(float(m[1][1]))
-        x.append(np.array(list(dfs[i]["measurement"].to_numpy())))
-
-delta = np.array(delta)
-x = torch.from_numpy(np.array(x))
-
-sigmas = (x - 0.5)[:, :, None, :]
-
-
-idcs = np.indices((1, 8))
-cb_A = idcs.sum(0) % 2
-cb_B = (idcs.sum(0) + 1) % 2
-
-stagm = torch.abs((sigmas * cb_A - sigmas * cb_B).mean((-1, -2)))
-
-
-# plt.plot(delta, stagm.mean(-1))
-# plt.fill_between(
-#     delta,
-#     stagm.mean(-1) + stagm.std(-1) / np.sqrt(stagm.shape[-1]),
-#     stagm.mean(-1) - stagm.std(-1) / np.sqrt(stagm.shape[-1]),
-#     alpha=0.5,
-# )
-
-stagm = x.sum(-1)
-
-plt.plot(delta, stagm.mean(-1))
-plt.fill_between(
-    delta,
-    stagm.mean(-1) + stagm.std(-1) / np.sqrt(stagm.shape[-1]),
-    stagm.mean(-1) - stagm.std(-1) / np.sqrt(stagm.shape[-1]),
-    alpha=0.5,
-)
+process_qmc_data(qmc_path, save_path=save_path)
