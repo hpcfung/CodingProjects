63bffbde3dfd7c0211be235b725ce2cb9e7c2ba7
hpcfung <91354621+hpcfung@users.noreply.github.com>
2023-06-28 22:03:35 -0400

    Update Reformat_BloqadeQMC_Data.py

--- 2023/USRA/qmc_script/processing/Reformat_BloqadeQMC_Data.py
+++ 2023/USRA/qmc_script/processing/Reformat_BloqadeQMC_Data.py
@@ -37,6 +37,13 @@ import json
 
 ########################################################################################
 def slicing_snake_flip(x):
+    """
+    [[1 2 3 4]
+     [5 6 7 8]]
+    to
+    [[1 2 3 4]
+     [8 7 6 5]]
+    """
     _x = copy.deepcopy(x)
     for i in range(_x.shape[-2]):
         if i % 2 == 1:
@@ -58,6 +65,7 @@ def process_qmc_batch(batch_dir):
         )
         # decorrelate
         batch_measurements = batch_measurements[::decorrelation_rate,:]
+
         # snake flip (measurements, spins)
         # start = time.time()
         batch_measurements = np.reshape(batch_measurements, (batch_measurements.shape[0],L,L))
@@ -68,9 +76,7 @@ def process_qmc_batch(batch_dir):
 
         measurements_list.append(batch_measurements)
     measurements = np.concatenate(measurements_list, axis=0)    
-    print(measurements)
-    print(measurements.shape)
-    sys.exit()
+    return measurements
 
 def process_qmc_data(qmc_path, save_path):
     """
@@ -87,36 +93,10 @@ def process_qmc_data(qmc_path, save_path):
                 batch_dir = os.path.join(qmc_path,"Rb="+rb_str,"omega="+omega_str,
                                "delta="+delta_str,"beta="+beta_str,
                                "seed="+seed_str)
-                process_qmc_batch(batch_dir=batch_dir)
-                sys.exit()
-    sys.exit()
-
-    for directory, subdirlist, filelist in os.walk(data_path):
-        for file in filelist:
-            if file.endswith("samples.csv"):
-
-                sample_path = os.path.join(directory, file)
-
-
-                m = re.findall(r"(\w+)=([+-]?[0-9]*[.]?[0-9]*)", sample_path)
-
-                L = int(m[0][1])
-                delta = float(m[1][1])
-
-                print(
-                    "{:<80}".format(
-                        "\rCurrently processing L={}, delta={} data".format(L, delta)
-                    ),
-                    end="\n",
-                )
-                sys.stdout.flush()
-
-                ########################################################################################
-
-                measurements = np.genfromtxt(sample_path, delimiter=",", dtype=str)
-                measurements = np.array([i.split(" ") for i in measurements]).astype(
-                    float
-                )
+                measurements = process_qmc_batch(batch_dir=batch_dir)
+                Rb = rb_dict[rb_str]
+                delta = delta_dict[delta_str]
+                beta = beta_dict[beta_str]
 
                 df = pd.DataFrame(
                     {"measurement": list(measurements)},
@@ -127,13 +107,13 @@ def process_qmc_data(qmc_path, save_path):
                 ########################################################################################
 
                 graph_config = GridGraph(
-                    num_atoms=L,
+                    num_atoms=L**2,
                     graph_name="grid_graph",
-                    Rb=1.2,
+                    Rb=Rb,
                     delta=delta,
-                    omega=1.0,
-                    beta=1.0,
-                    n_rows=1,
+                    omega=omega,
+                    beta=beta,
+                    n_rows=L,
                     n_cols=L,
                 )
                 graph = get_graph(graph_config)
@@ -159,7 +139,7 @@ def process_qmc_data(qmc_path, save_path):
                         os.makedirs(save_path)
 
                     folder_path = os.path.join(
-                        save_path, "EssiQQurke_L={}_delta={}".format(L, delta)
+                        save_path, "BloqadeQMC_L={}_Rb={}_delta={}_beta={}".format(L, rb_str, delta_str, beta_str)
                     )
                     os.mkdir(folder_path)
 
@@ -173,49 +153,58 @@ def process_qmc_data(qmc_path, save_path):
                     with open(os.path.join(folder_path, "config.json"), "w") as f:
                         json.dump(config_dict, f)
 
+                    print(f"{folder_path} generated.")
+                    sys.exit()
+
     return
 
 
 ########################################################################################
-# module load python/3.10
-# python Reformat_BloqadeQMC_Data.py
 
-save_path = os.path.abspath("/SCRATCHTAINER/qmc_data/tmp_data")
-# "/home/hpcfung/scratch/qmc_data/tmp_data"
-# os.path.abspath("/home/hpcfung/scratch/qmc_data/data")
+if __name__ == "__main__":
+    """
+    Processes one L=X subdirectory at a time
 
-qmc_path = "/SCRATCHTAINER/qmc_data/L=5"
-L = 5
+    Use bash script (need apptainer)
+    """
 
-rb_dict = {"1.05": 1.05, "1.15": 1.15, "1.30": 1.30}
-delta_dict = {"-0.36": -0.364386792453,
-              "-0.13": -0.128537735849,
-              "0.93": 	0.932783018868,
-              "1.05": 1.05070754717,
-              "1.17": 1.16863207547,
-              "1.29": 	1.28655660377,
-              "1.52": 1.52240566038,
-              "1.76": 1.75825471698,
-              "2.94": 	2.9375,
-              "3.17": 3.1733490566}
-beta_dict = {"0.5": 0.5,
-             "1.0": 1.0,
-             "2.0": 2.0,
-             "4.0": 4.0,
-             "8.0": 8.0,
-             "16.0": 16.0,
-             "32.0": 32.0,
-             "48.0": 48.0,
-             "64.0": 64.0}
+    save_path = os.path.abspath("/SCRATCHTAINER/qmc_data/data")
+    # "/home/hpcfung/scratch/qmc_data/tmp_data"
+    # os.path.abspath("/home/hpcfung/scratch/qmc_data/data")
 
-omega_str = "1.00"
-seed_str = "1234"
+    qmc_path = "/SCRATCHTAINER/qmc_data/L=5"
+    L = 5
 
-max_batch = 10
-batch_digits = len(str(max_batch))
+    rb_dict = {"1.05": 1.05, "1.15": 1.15, "1.30": 1.30}
+    delta_dict = {"-0.36": -0.364386792453,
+                "-0.13": -0.128537735849,
+                "0.93": 	0.932783018868,
+                "1.05": 1.05070754717,
+                "1.17": 1.16863207547,
+                "1.29": 	1.28655660377,
+                "1.52": 1.52240566038,
+                "1.76": 1.75825471698,
+                "2.94": 	2.9375,
+                "3.17": 3.1733490566}
+    beta_dict = {"0.5": 0.5,
+                "1.0": 1.0,
+                "2.0": 2.0,
+                "4.0": 4.0,
+                "8.0": 8.0,
+                "16.0": 16.0,
+                "32.0": 32.0,
+                "48.0": 48.0,
+                "64.0": 64.0}
 
-decorrelation_rate = 10
+    omega_str = "1.00"
+    seed_str = "1234"
 
-########################################################################################
+    max_batch = 10
+    batch_digits = len(str(max_batch))
+    omega = float(omega_str)
 
-process_qmc_data(qmc_path, save_path=save_path)
+    decorrelation_rate = 10
+
+    
+
+    process_qmc_data(qmc_path, save_path=save_path)
