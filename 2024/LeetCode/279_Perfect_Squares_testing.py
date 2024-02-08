e57dac1d4613dea7bfb7bb1d96ca5050e4e74182
hpcfung <91354621+hpcfung@users.noreply.github.com>
2024-02-08 15:31:49 -0500

    Update 279_Perfect_Squares_testing.py

--- 2024/LeetCode/279_Perfect_Squares_testing.py
+++ 2024/LeetCode/279_Perfect_Squares_testing.py
@@ -1,25 +1,42 @@
+from matplotlib import pyplot as plt
+
 class Solution:
     def numSquares(self, n: int) -> int:
         from math import sqrt, floor, ceil
 
+        self.count = 0
         record = [[n, floor(sqrt(n))]]
         for depth in range(1, 5):
+            remaining_depth = 4 - depth + 1
             tmp_record = []
             for attempt in record:
                 remainder, max_sq = attempt[0], attempt[1]
                 ub = min(max_sq, floor(sqrt(remainder)))
-                lb = ceil(sqrt(remainder/4))
-                node = 0
-                print(f"lb {lb}, ub {ub}, tot {ub-lb+1}")
-                for k in range(ub, lb-1, -1):
-                    node += 1
-                    new_remainder = remainder - k*k
+                lb = ceil(sqrt(remainder / remaining_depth))
+                for k in range(ub, lb - 1, -1):
+                    new_remainder = remainder - k * k
+                    self.count += 1
+                    # print(self.count, remainder, k, new_remainder)
                     if new_remainder == 0:
-                        print(f"node = {node}")
                         return depth
                     tmp_record.append([new_remainder, k])
-                print(f"node = {node}")
-            record = tmp_record        
+            record = tmp_record
 
-mySol = Solution()
-print(mySol.numSquares(1000))
+
+if __name__ == '__main__':
+    max_n = 5000
+
+    all_counts = []
+    all_avgs = []
+    mySol = Solution()
+    _sum = 0
+    for k in range(1, max_n+1):
+        mySol.numSquares(k)
+        all_counts.append(mySol.count)
+        _sum += mySol.count
+        avg = _sum / k
+        all_avgs.append(avg)
+        print(k, mySol.count, avg)
+    plt.plot(range(1, max_n+1), all_counts)
+    plt.plot(range(1, max_n+1), all_avgs)
+    plt.show()
