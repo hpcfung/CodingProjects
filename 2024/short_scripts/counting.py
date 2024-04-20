8d54235b962ba9ccdde442279923334c8b1541d6
hpcfung <91354621+hpcfung@users.noreply.github.com>
2024-04-20 15:10:18 -0400

    Update counting.py

--- 2024/short_scripts/counting.py
+++ 2024/short_scripts/counting.py
@@ -1,3 +1,4 @@
+# use flag; 13 lines
 for row_idx in range(len(matrix)):
     widths = []
     counting = False
@@ -7,7 +8,22 @@ for row_idx in range(len(matrix)):
                 counting = False
                 widths.append((l_lim, i-1))
         elif num == '1':
-                counting = True
-                l_lim = i
+            counting = True
+            l_lim = i
     if counting:
         widths.append((l_lim, i))
+
+# remember last num; 10 lines
+# no additional code after loop
+# no need to reset flag after condition (need to reset last in each loop though)
+# access arr[i-1] instead of memorizing; slower?
+for row_idx in range(len(matrix)):
+    widths = []
+    last = '0'
+    for i, num in enumerate(matrix[row_idx]):
+        if last == '0':
+            if num == '1':
+                l_lim = i
+        elif num == '0':
+            widths.append((l_lim, i-1))
+        last = num
